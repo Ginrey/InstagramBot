975a5dcdbefcd378106e5d43698261d1acabfa3e
Ginrey <skew2@mail.ru>
2016-12-27 20:58:47 +0300

    SQL

---
+++ InstagramBot/Data/SQL/CheckPermissions.cs
@@ -0,0 +1,33 @@
+﻿namespace InstagramBot.Data.SQL
+{
+    public class CheckPermissions
+    {
+    /*    public Permissons Permissons { get; set; }
+        OOGHost Host { get; }
+
+        public CheckPermissions(OOGHost host)
+        {
+            Host = host;
+            Permissons = Check();
+        }
+
+        Permissons Check()
+        {
+            Host.ConnectionInfo.SqlDB.GetLicenseState(
+                Host.ConnectionInfo.AccountInfo.LicenseID,
+                Host.ConnectionInfo.AccountInfo.SoftwareID,
+                out Host.ConnectionInfo.AccountInfo.State);
+
+            Host.ConnectionInfo.SqlDB.GetLicenseTime(
+                Host.ConnectionInfo.AccountInfo.LicenseID,
+                Host.ConnectionInfo.AccountInfo.SoftwareID,
+                out Host.ConnectionInfo.AccountInfo.SoftTime);
+
+            if (!Host.ConnectionInfo.AccountInfo.State) return Permissons = Permissons.Block;
+
+            return Permissons = (Host.ConnectionInfo.AccountInfo.SoftTime - UnixTime.Now).Timestamp > 0
+                ? Permissons.Success
+                : Permissons.TimeOff;
+        }*/
+    }
+}
